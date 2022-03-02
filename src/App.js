769cd915e31813c32c437b8ee82300beb975ce28
M-N-RASHFORD <85489724+M-N-RASHFORD@users.noreply.github.com>
2022-03-02 15:20:18 +0300

    adding the signup page where we shall be adding the administrators from

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Signup}/>
+            <Route path="/login" component={Login}/>
+            <Route path="/home" component={Home}/>
+        </Switch>
+    </Router>
   );
 }
 
