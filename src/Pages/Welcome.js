6c3c2ca596a20c88b558adf503d6b59b696148a7
M-N-RASHFORD <85489724+M-N-RASHFORD@users.noreply.github.com>
2022-03-03 14:09:13 +0300

    making things right at the css files

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -4,14 +4,12 @@ import '../Styles/welcome.css';
 const Welcome = ({ match, history }) => {
     const name = match.params.name;
     return (
-        <div className="body-welcome">
-        <center className="">
+        <center>
             <h2>Welcome { name }</h2>
             <button type="button" onClick={() => {
                 history.push("/login");
             }}>Click here to continue</button>
         </center>
-        </div>
     );
 }
 
